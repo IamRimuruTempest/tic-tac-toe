2bbbf3859af60e703152f1bb7a7c900c9fadb090
unknown <iamtrebojotneivda07@gmail.com>
2024-08-26 21:09:35 +0800

    Update cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://tic-tac-toe-game-puce-two.vercel.app",
     optionsSuccessStatus: 200,
   })
 );
